afc476d85df04564746738e03a1069ca3074b2ad
Lokeshloki888 <153089030+Lokeshloki888@users.noreply.github.com>
2025-01-03 10:00:28 +0530

    Add files via upload

---
+++ demo5.java
@@ -0,0 +1,12 @@
+ public class demo5{
+    public  static void main(String []args){
+        String username = "username001";
+        String pass = "username_001";
+        while((username == "username001") && (pass == "username_001")) {
+            System.out.println("Happy New Year");
+            break;
+
+        }
+
+    }
+}
