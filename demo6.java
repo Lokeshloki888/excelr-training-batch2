96ffb658a018b1426bcf06b3233205c6749e6717
Lokeshloki888 <153089030+Lokeshloki888@users.noreply.github.com>
2025-01-03 10:15:46 +0530

    Add files via upload

---
+++ demo6.java
@@ -0,0 +1,11 @@
+public class demo6{
+    public static void main(String[] args) {
+        String username = "Lokesh";
+        String pass = "lokesh@123";
+        while ((username =="Lokesh") && (pass == "lokesh@123")) {
+            System.out.println("welcome");
+            break;
+        }
+
+    }
+}
