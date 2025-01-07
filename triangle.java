9e397bbc311dfbc762dafc983bb79db38cc1caa3
Lokeshloki888 <153089030+Lokeshloki888@users.noreply.github.com>
2025-01-07 10:17:34 +0530

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+class triangle {
+
+    
+    static double area(double h, double b)
+    {
+        
+        return (h * b) / 2;
+    }
+
+    
+    public static void main(String[] args)
+    {
+        
+        double h = 10;
+
+        
+        double b = 5;
+
+        
+        System.out.println("Area of the triangle: "
+                           + area(h, b));
+    }
+}
