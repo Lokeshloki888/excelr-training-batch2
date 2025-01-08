284aec04fa6e635eef4649953c5034e20ee4074d
Lokeshloki888 <153089030+Lokeshloki888@users.noreply.github.com>
2025-01-08 10:11:44 +0530

    Add files via upload

---
+++ ps002.java
@@ -0,0 +1,9 @@
+public class ps002{
+    public static void main(String[] args){
+        int num = 10;
+        for(int i = 1; i<=num; i++){
+            System.out.println(100*i);
+        }
+    }
+
+}
