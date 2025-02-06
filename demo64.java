6ae49d107074ce9dc8ab9563d03567449f4a8033
Lokeshloki888 <153089030+Lokeshloki888@users.noreply.github.com>
2025-02-06 10:02:59 +0530

    Add files via upload

---
+++ demo64.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class demo64 {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+
+        
+        System.out.println("Original ArrayList: " + arrayList);
+
+        
+        swapFirstAndLast(arrayList);
+
+        
+        System.out.println("Modified ArrayList: " + arrayList);
+    }
+
+    public static void swapFirstAndLast(ArrayList<Integer> list) {
+        if (list.size() > 1) {
+           
+            int first = list.get(0);
+            int last = list.get(list.size() - 1);
+
+            
+            list.set(0, last);
+            list.set(list.size() - 1, first);
+        }
+    }
+}
---
+++ demo66.java
@@ -0,0 +1,15 @@
+public class demo66 {
+    public static void main(String[] args) {
+        
+        int[] array = {1, 2, 3, 4, 5};
+
+        
+        printArrayWithIndices(array);
+    }
+
+    public static void printArrayWithIndices(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Index " + i + ": " + array[i]);
+        }
+    }
+}
